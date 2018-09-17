bc4aa48cd6cfe3d72777b8ef80d1ac9f0da065ad
Drew Immerman <drew@stateless.studio>
2018-09-17 09:42:36 -0400

    Server-Side Rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 		AppComponent
 	],
 	imports: [
-		BrowserModule
+		 BrowserModule.withServerTransition({ appId: 'justins-tour-of-tutors' })
 	],
 	providers: [],
 	bootstrap: [AppComponent]
